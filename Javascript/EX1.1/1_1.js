c706360b8f71b2a6e29b59909955fcb3e29ed2f5
michaltsruya <112945494+michaltsruya@users.noreply.github.com>
2022-09-19 08:28:18 +0300

    Add files via upload

---
+++ Javascript/EX1.1/1_1.js
@@ -0,0 +1,21 @@
+let a = "This is a string";
+let b =  2;
+let queen = {
+    name: "Elizabeth",
+    age: 96
+  };
+  let smart = true;
+  let null_var = null;
+  let stam = undefined;
+  let and = Symbol("&");
+  console.log(
+    typeof a,
+    typeof b,
+    typeof queen,
+    typeof smart,
+    typeof null_var,
+    typeof stam,
+    typeof and
+  );
+
+
